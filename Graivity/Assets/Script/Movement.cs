4dd2fdc56bc5492f5cbd30039391346b2c589422
RainyFluff <nikodemius.ivarsson@elev.ga.lbs.se>
2023-03-01 14:25:10 +0100

    anders elk

--- Graivity/Assets/Script/Movement.cs
+++ Graivity/Assets/Script/Movement.cs
@@ -7,9 +7,11 @@ public class Movement : MonoBehaviour
     Rigidbody rb;
     public float rbForce = 10;
     public float rbJumpForce = 20;
+    public float maxSpeed = 10;
     bool isGrounded;
     public GameObject feet;
     LayerMask groundMask;
+    Vector3 speedCap;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,7 +24,11 @@ public class Movement : MonoBehaviour
     {
         move();
         isGrounded = Physics.CheckSphere(feet.transform.position, 0.2f, groundMask);
-        Debug.Log(isGrounded);
+        speedCap = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
     }
 
     void move()
@@ -40,9 +46,23 @@ public class Movement : MonoBehaviour
         //jump
         if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && isGrounded)
         {
-            
             rb.AddForce(transform.up * rbJumpForce, ForceMode.Impulse);
         }
+        //slamdown
+        if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.LeftControl)) && !isGrounded)
+        {
+            rb.AddForce(-transform.up * rbJumpForce, ForceMode.VelocityChange);
+        }
+
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && isGrounded)
+        {
+            rb.drag = 10;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+
 
     }
 }
