ef15a1f0e13bb0f7e8c1a5f1a6fedff8f037d31b
Anders Brandt <75731366+Gunstringerlb@users.noreply.github.com>
2023-03-08 13:31:52 +0100

    WeaponLookAtMouse
    plebber

--- Graivity/Assets/Scripts/Weapon.cs
+++ Graivity/Assets/Scripts/Weapon.cs
@@ -6,13 +6,30 @@ public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
-   
+
+    private Camera mainCam;
+    private Vector3 mousePos;
+
+   void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
+
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rotation = mousePos - transform.position;
+
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+       
+
     }
 
     void Shoot()
