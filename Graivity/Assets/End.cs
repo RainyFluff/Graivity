83171056fddabb379730c43904201357272c74e3
Caian6904 <73233343+Caian6904@users.noreply.github.com>
2023-05-24 12:20:08 +0200

    Escape to quit

--- Graivity/Assets/End.cs
+++ Graivity/Assets/End.cs
@@ -14,7 +14,10 @@ public class End : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
